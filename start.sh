5eb87d3ecbd7ec266afe1c17a471901cfe762e00
Edward Li <edward.w.li@outlook.com>
2023-09-19 20:53:16 -0400

    Minor changes to control socket code

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
-user=edwarli
-host=greatlakes.arc-ts.umich.edu
-
-start_script=start-jupyterlab.sh
-slurm_script=jupyterlab.slurm
-
-control_socket=~/.ssh/control_%r@%h:%p
-
 usage() {
   echo "Usage: $0 <uniqname>"
 }
@@ -24,6 +16,12 @@ big_message() {
   echo
 }
 
+user=edwarli
+host=greatlakes.arc-ts.umich.edu
+
+start_script=start-jupyterlab.sh
+slurm_script=jupyterlab.slurm
+
 if [ "$#" -ne 1 ] && [ -z "${user}" ]
 then
   usage >&2
@@ -36,18 +34,28 @@ then
 fi
 
 location=${user}@${host}
+control_socket=~/.ssh/control_${location}
 
-big_message "ESTABLISHING CONNECTION" >&2
-ssh -fMN -S "${control_socket}" "${location}"
+if [ ! -S "${control_socket}" ]
+then
+  big_message "ESTABLISHING CONNECTION" >&2
+  ssh -fMN -S "${control_socket}" "${location}"
+fi
+
+big_message "STARTING SERVER" >&2
+sbatch_msg=$(ssh -S "${control_socket}" "${location}" sbatch < "${slurm_script}")
 
 big_message "GETTING SERVER INFORMATION" >&2
-job=$(ssh -S "${control_socket}" "${location}" sbatch < "${slurm_script}" | grep -F "Submitted batch job" | sed -n 's/Submitted batch job \([[:digit:]]*\)/\1/p')
+job=$(echo "${sbatch_msg}" | grep -F "Submitted batch job" | sed -n 's/Submitted batch job \([[:digit:]]*\)/\1/p')
 start_log=$(ssh -S "${control_socket}" "${location}" "sh -s" < "${start_script}" "${job}")
 node=$(echo "${start_log}" | grep -F "node =" | sed -n 's/node = \(.*\)/\1/p')
 port=$(echo "${start_log}" | grep -F "port =" | sed -n 's/port = \(.*\)/\1/p')
+log=$(echo "${start_log}" | grep _F "log =" | sed -n 's/log = \(.*\)/\1/p')
 
-big_message "JOBID = ${job}\nGO TO http://localhost:${port}\nENTER Ctrl-c TO STOP" >&2
-ssh -S "${control_socket}" -N -L "${port}:${node}:${port}" "${location}"
+big_message "JOBID = ${job}\n\nGO TO http://localhost:${port}\n\nENTER Ctrl-c TO STOP" >&2
+
+big_message "PORT FORWARDING" >&2
+ssh -N -S "${control_socket}" -L "${port}:${node}:${port}" "${location}"
 
 big_message "STOPPING SERVER"
 ssh -S "${control_socket}" "${location}" scancel "${job}"
